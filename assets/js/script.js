f5d97da91bf50e52df9d5414a7298b044cde3821
Pawel Kojs <pawel.kojs@gmail.com>
2026-06-26 02:01:58 +0200

    Add Twin Club certificate material

--- assets/js/script.js
+++ assets/js/script.js
@@ -796,7 +796,7 @@
 
 /* ===== Sprint 1.1 — pilotaż warstwy danych JSON ===== */
 (function () {
-  var DATA_VERSION = "1.5.124";
+  var DATA_VERSION = "1.5.125";
 
   function assetDataUrl(fileName) {
     var script = document.currentScript || document.querySelector('script[src*="assets/js/script.js"]');
